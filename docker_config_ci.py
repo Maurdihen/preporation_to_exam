69eca40e9c6d18fd6255404fbd9bdf61b87e1f48
egorarhipov <arhipov_0406@mail.ru>
2023-05-30 12:08:07 +0300

    commite 7

--- docker_config_ci.py
+++ docker_config_ci.py
@@ -1,1 +1,6 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://flask_app:flask_app_password@pg/flask_app"
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@pg/$DB_NAME"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
+    JSON_AS_ASCII = False
